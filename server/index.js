b29e1576fb429fbaa6882da9aea5837f0170e9f7
misprachi023 <prachimishra23123@gmail.com>
2024-02-12 02:47:56 +0530

    add DEPLOY LINK

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({ 
-		origin: ["http://127.0.0.1:5173"],
+		origin: ["http://127.0.0.1:5173","https://boat-life-style-nine.vercel.app"],
 		credentials: true,
 	})
 );
